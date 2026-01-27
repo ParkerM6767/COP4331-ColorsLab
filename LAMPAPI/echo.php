dd6dcf994cdb73819628a6e23ea4f6ca3f829385
Stephen M <smeikle1@gmail.com>
2026-01-27 17:26:50 -0500

    Intial commit of file structure

---
+++ LAMPAPI/echo.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo 'Method Not Allowed';
+    exit;
+}
+
+$value = isset($_GET['echo']) ? $_GET['echo'] : http_response_code(400) && exit('Bad Request: Missing "echo" parameter');
+echo htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
