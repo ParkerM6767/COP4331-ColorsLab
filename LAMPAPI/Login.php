741c92985ccdcc1fa86b496ac15ff1b79aad432b
ParkerM6767 <parker@lamay.systems>
2026-01-30 13:30:32 -0500

    change ignore and made route for Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,16 +1,59 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
+$config = parse_ini_file("../db_config.ini");
+
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
     echo 'Method Not Allowed';
     exit;
 }
 
-$jsonBody = file_get_contents('php://input');
-$data = json_decode($jsonBody, true);
+$db = mysqli("localhost", $config['DB_USER'], $config['DB_PASSWORD'], "COP4331");
 
-echo "<pre>";
-print_r($data['username']);
-echo "</pre>";
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $jsonBody = file_get_contents('php://input');
+    $inData = json_decode($jsonBody, true);
+
+
+    $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+    $stmt->bind_param("ss", $inData["username"], $inData["password"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($row = $result->fetch_assoc()) {
+        returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
+    } else {
+        returnWithError("No Records Found");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($firstName, $lastName, $id)
+{
+    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
+
 ?>
