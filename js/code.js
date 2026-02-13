5fdae01cec46b4d160ad2ab26e605da19bebeb10
ParkerM6767 <parker@lamay.systems>
2026-02-13 12:30:18 -0500

    Added functionality for client-side password hashing using md5

--- js/code.js
+++ js/code.js
@@ -6,14 +6,47 @@ async function doLogin() {
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
     }
+
+    // hashing password test
+    var hash = md5(data.password)
+
+    data.password = hash;
+
     let req = new XMLHttpRequest()
 
     req.open("POST", api_url + 'Login.php')
     req.setRequestHeader("Content-Type", "application/json", "charset=utf-8")
-    req.send(JSON.stringify(data))
-}
 
+    try
+	{
+		req.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( req.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = `
+                    <div style="width:35%;margin: auto;" role="alert" class="alert alert-danger text-center mb-5">
+                        Login invalid: username/password incorrect.
+                    </div>`;
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
 
-async function doLogout() {
-    
+				saveCookie();
+	
+				window.location.href = "color.html";
+			}
+		};
+		req.send(JSON.stringify(data));
+	}
+	catch(err)
+	{
+		console.warn(err.message)
+	}
 }
