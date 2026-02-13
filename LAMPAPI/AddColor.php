4db7441ce875c8c39017022d8ddc256c9ee64374
ParkerM6767 <parker@lamay.systems>
2026-02-13 12:23:15 -0500

    Fixed syntax error and pathing to environment variables

---
+++ LAMPAPI/AddColor.php
@@ -0,0 +1,37 @@
+<?php
+$env = parse_ini_file("../db_config.ini");
+$inData = getRequestInfo();
+
+$color = $inData["color"];
+$userId = $inData["userId"];
+
+$conn = new mysqli("localhost", $env["DB_USER"], $env["DB_PASSWORD"], $env["DB_NAME"]);
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+	$stmt->bind_param("ss", $userId, $color);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
+?>
--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,59 +1,52 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
 
-$config = parse_ini_file("../db_config.ini");
+$env = parse_ini_file("../db_config.ini");
+$inData = getRequestInfo();
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo 'Method Not Allowed';
-    exit;
-}
+$id = 0;
+$firstName = "";
+$lastName = "";
 
-$db = mysqli("localhost", $config['DB_USER'], $config['DB_PASSWORD'], "COP4331");
-
+$conn = new mysqli("localhost", $env["DB_USER"], $env["DB_PASSWORD"], $env["DB_NAME"]);
 if ($conn->connect_error) {
-    returnWithError($conn->connect_error);
+	returnWithError($conn->connect_error);
 } else {
-    $jsonBody = file_get_contents('php://input');
-    $inData = json_decode($jsonBody, true);
+	$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+	$stmt->bind_param("ss", $inData["username"], $inData["password"]);
+	$stmt->execute();
+	$result = $stmt->get_result();
 
+	if ($row = $result->fetch_assoc()) {
+		returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
+	} else {
+		returnWithError("No Records Found");
+	}
 
-    $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-    $stmt->bind_param("ss", $inData["username"], $inData["password"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($row = $result->fetch_assoc()) {
-        returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
-    } else {
-        returnWithError("No Records Found");
-    }
-
-    $stmt->close();
-    $conn->close();
+	$stmt->close();
+	$conn->close();
 }
 
 function getRequestInfo()
 {
-    return json_decode(file_get_contents('php://input'), true);
+	return json_decode(file_get_contents('php://input'), true);
 }
 
 function sendResultInfoAsJson($obj)
 {
-    header('Content-type: application/json');
-    echo $obj;
+	header('Content-type: application/json');
+	echo $obj;
 }
 
 function returnWithError($err)
 {
-    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-    sendResultInfoAsJson($retValue);
+	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
 }
 
 function returnWithInfo($firstName, $lastName, $id)
 {
-    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-    sendResultInfoAsJson($retValue);
+	$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+	sendResultInfoAsJson($retValue);
 }
 
 ?>
